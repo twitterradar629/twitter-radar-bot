dd69e7c6813965755aa657bd6b233c5c9e734461
twitterradar629 <72346714+twitterradar629@users.noreply.github.com>
2020-10-05 19:55:13 +0545

    Update puppeteer.js

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -1,1 +1,20 @@
+const puppeteer = require('puppeteer');
 
+async function main() {
+  console.log('Browser is launching...');
+  const browser = await puppeteer.launch();
+  
+  const page = await browser.newPage();
+
+  let url = "https://radar.cloudflare.com/us";
+  console.log(`Browsing url: ${url}`);
+  await page.goto(url, { waitUntil: 'networkidle2' })
+  await page.waitForTimeout(10000)
+  
+  console.log('Capturing screenshot...');
+  await page.screenshot({path: 'screenshot.png', fullPage: true})
+    
+  browser.close()  
+}
+
+main()
