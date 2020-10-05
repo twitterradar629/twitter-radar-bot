78f960599bc27607e031f176ce8a8a41fe10c3c8
twitterradar629 <72346714+twitterradar629@users.noreply.github.com>
2020-10-05 21:00:05 +0545

    Update puppeteer.js

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -1,21 +1,9 @@
 const puppeteer = require('puppeteer');
 
-async function take_screenshot_of_popular_domains(page) {
-    console.log('📸 Taking screenshot of "Most Popular Domains in US"');
-    const filename = 'screenshots/popular_domains.png';
-  
-    const selector = '#root > article > div > div:nth-child(4) > div.eg.av > div.fo.ev.bd'
-
-    const element = await page.$(selector)
-
-    await element.screenshot({ path: filename });
-    console.log(`🗾 Screenshot saved as ${filename}`)
-}
-
 async function main() {
   console.log('Browser is launching...');
   const browser = await puppeteer.launch();
-  
+
   const page = await browser.newPage();
 
   let url = "https://radar.cloudflare.com/us";
@@ -23,7 +11,8 @@ async function main() {
   await page.goto(url, { waitUntil: 'networkidle2' })
   await page.waitForTimeout(10000)
 
-  await take_screenshot_of_popular_domains(page)
+  console.log('Capturing screenshot...');
+  await page.screenshot({path: 'screenshot.png', fullPage: true})
 
   browser.close()  
 }
